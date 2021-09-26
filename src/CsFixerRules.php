bf4c5191bd522e2bf941eba960dfbf163fbe7dc6
Andre Polykanine <ap@oire.me>
2021-09-26 16:09:26 +0200

    Remove another obsolete rule

--- src/CsFixerRules.php
+++ src/CsFixerRules.php
@@ -270,7 +270,6 @@ final class CsFixerRules
         'ternary_operator_spaces' => true,
         'ternary_to_elvis_operator' => true,
         'ternary_to_null_coalescing' => true,
-        'trailing_comma_in_multiline_array' => false,
         'trim_array_spaces' => true,
         'types_spaces' => ['space' => 'none'],
         'unary_operator_spaces' => true,
